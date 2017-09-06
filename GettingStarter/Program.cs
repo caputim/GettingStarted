3a49a1333d15af5a143a0818f774df5a99894d51
Michael Caputi <1caputim@gmail.com>
2017-09-06 11:28:08 -0400

    I tried
    f

--- GettingStarter/Program.cs
+++ GettingStarter/Program.cs
@@ -11,6 +11,19 @@ namespace GettingStarter
         static void Main(string[] args)
         {
             Console.WriteLine("hello");
+            String correctu = "root";
+            String correctp = "letmein";
+            String attemptu = "";
+            String attemptp = "";
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter a username: ");
+                attemptu = Console.ReadLine;
+
+                Console.Write("Enter a password: ")
+                    attemptp = Console.ReadLine;
+
+                    }
         }
     }
 }
